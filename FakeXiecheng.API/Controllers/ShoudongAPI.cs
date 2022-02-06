691a4b06a424ce8af1f4746fe70a4a0024725c83
Charles Deng <dengcharles@hotmail.com>
2022-02-06 15:50:04 +1100

    InitialCommit

---
+++ FakeXiecheng.API/Controllers/ShoudongAPI.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FakeXiecheng.API.Controllers
+{
+    [Route("api/shoudongapi")]
+    //[ApiController]
+    public class ShoudongAPIController
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "my value1", "my value2" };
+        }
+    }
+}
